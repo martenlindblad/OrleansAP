7854d62d05f907b8624c8de76aecac94f307f2f0
Mårten Lindblad <marten.lindblad@acando.com>
2017-03-07 10:16:54 +0100

    Added Google Analytics Interface and Class

---
+++ src/grains/GoogleAnalyticsGrain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using OrleansAP.Interfaces;
+
+namespace OrleansAP.Grains
+{
+    public class GoogleAnalyticsGrain : Orleans.Grain, IGoogleAnalyticsGrain
+    {
+        Task<string> IGoogleAnalyticsGrain.SayHello(string websiteCode)
+        {
+            return Task.FromResult("You said: '" + websiteCode + "', I say: Hello!");
+        }
+    }
+}
---
+++ src/interfaces/IGoogleAnalyticsGrain.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OrleansAP.Interfaces
+{
+    public interface IGoogleAnalyticsGrain : Orleans.IGrainWithGuidKey
+    {
+        Task<string> SayHello(string websiteCode);
+    }
+}
